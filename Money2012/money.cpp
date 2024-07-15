f0b61576ee82e824bcfe6f49b07cf2b63fb92836
Dmytro Skorokhod <dima.skorokhod@gmail.com>
2024-07-15 10:28:27 +0300

    first commit

---
+++ Money2012/money.cpp
@@ -0,0 +1,8 @@
+//
+//  money.cpp
+//  Money2012
+//
+//  Created by Dmytro Skorokhod on 15/07/2024.
+//
+
+#include "money.hpp"
---
+++ Money2012/money.hpp
@@ -0,0 +1,13 @@
+//
+//  money.hpp
+//  Money2012
+//
+//  Created by Dmytro Skorokhod on 15/07/2024.
+//
+
+#ifndef money_hpp
+#define money_hpp
+
+#include <stdio.h>
+
+#endif /* money_hpp */
